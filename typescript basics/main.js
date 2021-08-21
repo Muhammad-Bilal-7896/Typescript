25712950b436fd46160c2cb5b9bfebe1a1ecef08
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-08-21 14:56:19 +0500

    done uptil mixed arrays

--- typescript basics/main.js
+++ typescript basics/main.js
@@ -9,6 +9,22 @@ var sum;
 var title = 'BilalMohib';
 var isBegginer = true;
 var total = 0;
-var name = 'Bilal';
+var name = 'Pakistan';
 var status = name + " is Programming and learning with typescript";
 console.log(status);
+//Null definations
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+//Arrays in typescript
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+///////////////////Array with values of mixed type/////////////////////
+//Note that the number of values and declared types should be the same
+//i.e If there are two definations string and number then things to remember are:
+//1) Only array will contain two elements
+//2) The order will be same i.e one string and one number
+//3) The number cant come first the order will be followed as its defined
+//So here is an example how would we do that.
+var mixedArray = ['Chris', 22];
--- typescript basics/main.ts
+++ typescript basics/main.ts
@@ -20,3 +20,24 @@ let name : string = 'Pakistan';
 let status:string = `${name} is Programming and learning with typescript`;
 
 console.log(status);
+
+
+//Null definations
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName:string = undefined;
+
+//Arrays in typescript
+let list1: number[] = [1,2,3];
+let list2:Array<number> = [1,2,3];
+
+///////////////////Array with values of mixed type/////////////////////
+//Note that the number of values and declared types should be the same
+//i.e If there are two definations string and number then things to remember are:
+//1) Only array will contain two elements
+//2) The order will be same i.e one string and one number
+//3) The number cant come first the order will be followed as its defined
+//So here is an example how would we do that.
+let mixedArray:[string,number] = ['Chris',22];

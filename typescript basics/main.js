ac8e4572dcd7e8ba601295ddaff83c42ec082429
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-08-21 13:26:17 +0500

    initializede

---
+++ typescript basics/main.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var message = "sdafsdf";
+console.log(message);
+//Variable Declarations
+var x = 10;
+var y = 20;
+var sum;
+var title = 'BilalMohib';
---
+++ typescript basics/main.ts
@@ -0,0 +1,14 @@
+export {}
+
+let message = "sdafsdf";
+
+console.log(message);
+
+
+//Variable Declarations
+let x = 10;
+const y = 20;
+
+let sum;
+
+const title = 'BilalMohib';

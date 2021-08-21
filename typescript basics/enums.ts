70e1242cd562ccb72e6985a7404caa8d8598d1e5
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-08-21 18:31:07 +0500

    feat

---
+++ typescript basics/enums.ts
@@ -0,0 +1,5 @@
+//Enums in typescript
+
+//This export is added because typescript is a language not a scripting language so it treats
+//every .ts file as a module instead of a script so we export nothing so the module exports nothing
+export {}
--- typescript basics/main.ts
+++ typescript basics/main.ts
@@ -1,3 +1,5 @@
+//This export is added because typescript is a language not a scripting language so it treats
+//every .ts file as a module instead of a script so we export nothing so the module exports nothing
 export {}
 
 let message = "sdafsdf";

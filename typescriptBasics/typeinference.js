a836c74831ab7f4579b44b82182f7961f0b7e8a3
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-25 01:35:21 +0500

    feat

--- typescriptBasics/typeinference.js
+++ typescriptBasics/typeinference.js
@@ -1,7 +1,5 @@
-//This export is added because typescript is a language not a scripting language so it treats
-//every .ts file as a module instead of a script so we export nothing so the module exports nothing
-export {}
-
+"use strict";
+exports.__esModule = true;
 //Type inference
 //So basically the concept of type inference is that 
 //In typescript sometimes you forgot to declare the type of variable or means you forgot to specify the type 
@@ -10,13 +8,10 @@ export {}
 //But if you are defining a variable and also initializing it while defining so what happens is that
 //Type script automatically infers i.e assumes a type specific the one with which you initialzed the variable while defining so that variable will have that type so its not possible to redefine that variable to any other data type value
 //Thats all
-
-let a;
+var a;
 a = 10;
 a = true;
-
 //This demonstrates the type inference
 //let b = 20;
 //b = true;
-
 console.log(a);
